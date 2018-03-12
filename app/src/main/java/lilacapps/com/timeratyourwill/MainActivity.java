4767d320106f034f6bceeef2890b7e7dcd65f8f2
Vishnu Priya Nallan <VishnuPriya.Nallan@jda.com>
2018-03-12 16:26:00 +0530

    Need cleanup

---
+++ app/src/main/java/lilacapps/com/timeratyourwill/MainActivity.java
@@ -0,0 +1,29 @@
+package lilacapps.com.timeratyourwill;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.menu,menu);
+
+        return true;
+    }
+}
